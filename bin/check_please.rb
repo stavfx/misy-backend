fbc7795dcddcf0b8ca6858bb836483c5a8da0499
Stav Raviv <stav.raviv@avg.com>
2015-05-13 02:26:53 +0300

    .

--- bin/check_please.rb
+++ bin/check_please.rb
@@ -2,13 +2,13 @@ require 'rubygems'
 require 'sinatra'
 Dir['../lib/*.rb'].each {|file| require file }
 
-require 'mongo'
-puts Gem.loaded_specs["mongo"].version
+#require 'mongo'
+#puts Gem.loaded_specs["mongo"].version
 
-mongo_uri = ENV['MONGOLAB_URI']
-client = Mongo::Client.new(mongo_uri);
-db = client.database
-db.collection_names.each{|name| puts name }
+#mongo_uri = ENV['MONGOLAB_URI']
+#client = Mongo::Client.new(mongo_uri);
+#db = client.database
+#db.collection_names.each{|name| puts name }
 
 get '/' do
   "Hello World!"
