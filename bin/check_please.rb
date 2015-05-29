21d3a2e9e0be49ea7d5f59627b949dce5bae3a32
stavfx <stavfx@gmail.com>
2015-05-29 17:31:47 +0300

    Update check_please.rb

--- bin/check_please.rb
+++ bin/check_please.rb
@@ -9,6 +9,9 @@ enable :sessions
 set :bind, '0.0.0.0'
 #set :port, 80
 
+
+
+#
 # MongoMapper.connection = Mongo::Connection.new(host, port)
 # MongoMapper.database = db_name
 # MongoMapper.database.authenticate(db_name, pw)
