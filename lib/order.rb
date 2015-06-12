24a7760733ddda0580c7aefe684954f86eb9a3a2
Jasmine Nouriel <jasmine.nouriel@gmail.com>
2015-06-12 16:40:37 +0300

    Changed all to where

--- lib/order.rb
+++ lib/order.rb
@@ -81,7 +81,8 @@ class OrderMng
 
   def self.get_orders_history_by_user(user_id)
     orders_by_session = Hash.new {|h,k| h[k] = {"menu_items" => [],"user_id" => user_id, "date" => Time.now.to_i } }  # Hash of hashes
-    Order.all(:user_id => user_id, :menu_items => { :$exists => true}, :menu_items => {:$not => {:$size => 0}}).each do |order|
+    puts user_id
+    Order.where(:user_id => user_id, :menu_items => { :$exists => true}, :menu_items => {:$not => {:$size => 0}}).each do |order|
       puts order.dining_session
       orders_by_session[order.dining_session]["menu_items"] =+ order.menu_items
       orders_by_session[order.dining_session]["restaurant_id"] ||= order.restaurant_id
