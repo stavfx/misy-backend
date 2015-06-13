21ca8c5a1edaa2d6d7ee574647140ade5079c834
Jasmine Nouriel <jasmine.nouriel@gmail.com>
2015-06-13 16:44:37 +0300

    Add categories to opening data

--- lib/utils.rb
+++ lib/utils.rb
@@ -14,5 +14,6 @@ def get_opening_data(user_id = nil)
   data["restaurants"] = (RestaurantMng.get_all)[:data] if user_id.nil?
   data["cities"] = (RestaurantMng.get_all_cities)[:data]
   data["admin_of_restaurant"] = RestaurantMng.get_restaurant_by_user(user_id)[:data] unless user_id.nil?
+  data["menu_categories"] = MenuItemMng.get_all_menu_categories[:data]
   return data
 end
