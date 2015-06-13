fd0cc60d0179f9eaa6a07621810beb0e58893e5d
Maayan Apelboim <mapelboim@gmail.com>
2015-06-13 13:18:45 +0300

    runApriri - now working

--- algo/runApriori.rb
+++ algo/runApriori.rb
@@ -41,7 +41,7 @@ def runApriori(restid,userOrders)
   outputArray = []
   orderedItems = getHash(restid)
   orderedItems = Apriori::ItemSet.new(orderedItems)
-  outA = orderedItems.mine(60, 60)
+  outA = orderedItems.mine(30, 60)
   # get all menu items of specific restaurant
   restItems=RestaurantMng.get_all_menu_items(restid)
 
