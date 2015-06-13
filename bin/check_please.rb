f4d3d76f3351543515c3addab96f5d9a783d6c51
Jasmine Nouriel <jasmine.nouriel@gmail.com>
2015-06-13 17:13:56 +0300

    upload photo

--- bin/check_please.rb
+++ bin/check_please.rb
@@ -193,3 +193,17 @@ get '/api/test' do
   file = File.join('/path', file)
   send_file(file, :disposition => 'attachment', :filename => File.basename(file))
 end
+
+
+# Handle GET-request (Show the upload form)
+get "/upload" do
+  haml :upload
+end
+
+# Handle POST-request (Receive and save the uploaded file)
+post "/upload" do
+  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+    f.write(params['myfile'][:tempfile].read)
+  end
+  return "The file was successfully uploaded!"
+end
